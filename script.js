469bfed0892d74c1b7f1b0aeecd09f3d32c68bdf
Ricwolf19 <rhtc19@gmail.com>
2023-11-10 16:54:20 -0600

    feat: random number and input number

---
+++ script.js
@@ -0,0 +1,9 @@
+//Get the random number with Math
+let randomNum = Math.floor((Math.random() * 101) + 1);
+
+//In this way you can save the number in the input of the html
+let inputNum = document.getElementById('inputNum');
+
+
+//Function that checks the number entered and compares to the correct result
+const checkResult = () => num*8;
